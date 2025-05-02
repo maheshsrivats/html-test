bf3efcd63d1568b4bd0fcb157b3ba1241009a35e
maheshsrivats <71691128+maheshsrivats@users.noreply.github.com>
2025-05-02 16:42:32 +0530

    Update and rename README.md to setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='your-python-project',
+    version='1.0.0',
+    description='A simple Python project for Azure Static Web Apps',
+    py_modules=['test_flask'],
+    install_requires=[
+        'flask',
+    ],
+)
